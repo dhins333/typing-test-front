4cd6df9cda349654145046d18ae181865e0c2445
Dhinesh Kumar <dhins333@gmail.com>
2020-08-18 14:11:37 +0530

    build1

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter,Switch,Route } from 'react-router-dom';
 import Header from './components/Header';
 import MainPage from './components/MainPage';
+import ErrorPage from './components/404Page';
 
 const AppRouter = () => {
     return(
@@ -9,6 +10,7 @@ const AppRouter = () => {
         <Header/>
             <Switch>
                 <Route component={MainPage} path='/' exact={true}/>
+                <Route component={ErrorPage} path = '*'/>
             </Switch>
         </BrowserRouter>
     )
